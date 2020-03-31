b525172f41462ca6ab8903bb6b2cbfecd768fdf4
tindrew <tindrewmar@gmail.com>
2020-03-31 15:24:10 -0500

    initial commit of the bootcamp files

---
+++ basics/numbers.js
@@ -0,0 +1,12 @@
+let age = 42
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+ let studentScore = 69
+ let maxScore = 70
+ let percent = (studentScore / maxScore) * 100
+
+ console.log(percent)
+
+
---
+++ basics/strings.js
@@ -0,0 +1,17 @@
+let firstName = 'Andrew'
+let lastName = 'Martin'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+
+let city = 'Mountain Home'//country
+let state = 'Arkansas'
+let country = 'United States'
+
+let location = city + ', ' + state + ', ' + country
+
+console.log(location)
+
+
+
+// Mountain Home, Arkansas, United states
