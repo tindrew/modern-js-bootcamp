8ec2687a17276d6ab1ff814f3577826d79e0d7ef
tindrew <tindrewmar@gmail.com>
2020-03-31 21:01:07 -0500

    added more exercises

---
+++ basics/boolean.js
@@ -0,0 +1,21 @@
+let temp = 31
+let isFreezing = temp < 66
+
+console.log(isFreezing)
+
+// Challenge area
+
+let age = 5
+let isChild =  age < 10
+let isSenior = age >= 65
+if (age < 10) {
+    console.log("isChild is: " + isChild)
+} else if (age >= 65) {
+    console.log("isSenior = " + isSenior)
+}
+
+
+// calculate is senior
+//print is child value
+// print is senior value
+
