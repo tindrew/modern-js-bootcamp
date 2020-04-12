6e54d80ceb2db25eb62f4c8dfb77aa953c17dd3f
tindrew <tindrewmar@gmail.com>
2020-04-12 15:02:24 -0500

    added another button

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -12,3 +12,7 @@ const notes = [{
 document.querySelector('button').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'
 })
+
+document.querySelectorAll('button')[1].addEventListener('click', function() {
+    console.log('delete all notes')
+})
